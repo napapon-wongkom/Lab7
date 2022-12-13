166e82b49d7a03cc1fbc03455d4b2124bed0043b
Napapon-Wongkom <dot.wongkom@gmail.com>
2022-12-13 17:12:39 +0700

    lab7_4.cpp

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -10,6 +10,15 @@ int main(){
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
     //Write your code here.
-		
+	double num_gpa;
+	int i =0;
+	cout<<"Please input GPA: ";
+	cin>>num_gpa;
+	while(i<=9){
+		if(gpa[i]>=num_gpa){
+			cout<<id[i]<<" "<<name[i]<<" "<<gpa[i]<<"\n";
+		}
+		i++;
+	}
 	return 0;
 }
