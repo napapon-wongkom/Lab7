e55347df2548e5801277b383bd8cc4dab7af90f8
Napapon-Wongkom <dot.wongkom@gmail.com>
2022-12-13 16:59:31 +0700

    lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,36 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
-
+int adiff(int a, int b){
+  int circle =360;
+  int ans;
+  int roundcount_a =a/circle;
+  int true_a =a-(circle*roundcount_a);
+  int roundcount_b = b/circle;
+  int true_b =b-(circle*roundcount_b);
+  if(true_a>true_b){
+    ans=true_a-true_b;
+    if(ans>180){
+      ans=360-ans;
+    }
+  }
+  else{
+    ans=true_b-true_a;
+    if(ans>180){
+      ans=360-ans;
+    }
+  }
+  return ans;
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
+  cout << adiff(180,270)<<"\n";
+  cout << adiff(210,45)<<"\n";
+  cout << adiff(0,360)<<"\n";
+  cout << adiff(10,350)<<"\n";
+  cout << adiff(95,260)<<"\n";
+  cout << adiff(90,-90)<<"\n";
+  cout << adiff(1000,280)<<"\n";
   cout << adiff(60,244);
 }
